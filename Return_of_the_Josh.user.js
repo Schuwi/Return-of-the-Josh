c2a6c556be879e8bbc6f03d7991b556f1289ed46
Schuwi <schuwi4@gmail.com>
2015-04-07 16:42:37 +0200

    Update 1.2
    Added Twitter checking on @proceduralguy and @LimitTheory
    Added caching of the results so new info is only loaded every half an hour

--- Return_of_the_Josh.user.js
+++ Return_of_the_Josh.user.js
@@ -6,17 +6,45 @@
 // @updateURL	  https://github.com/Schuwi/Return-of-the-Josh/raw/master/Return_of_the_Josh.user.js
 // @downloadURL	https://github.com/Schuwi/Return-of-the-Josh/raw/master/Return_of_the_Josh.user.js
 // @include     http://forums.ltheory.com*
-// @version     1.1
+// @version     1.2
 // @grant       GM_xmlhttpRequest
+// @grant       GM_setValue
+// @grant       GM_getValue
 // ==/UserScript==
 
 var kickstarter_link = 'https://www.kickstarter.com/projects/joshparnell/limit-theory-an-infinite-procedural-space-game/posts';
 var forum_link = 'http://forums.ltheory.com/search.php?keywords=&terms=all&author=JoshParnell&sc=1&sf=all&sr=posts&sk=t&sd=d&st=0&ch=300&t=0&submit=Search';
+var twitter_josh_link = 'https://twitter.com/proceduralguy';
+var twitter_lt_link = 'https://twitter.com/LimitTheory';
 
-var new_forum_post = false;
-var new_kickstarter_post = false;
-var forum_loaded = false;
-var kickstarter_loaded = false;
+var kickstarter_ref = '<a style="color: #264089;" href="' + kickstarter_link + '" target="_blank">Kickstarter</a>';
+var forum_ref = 'the <a style="color: #264089;" href="' + forum_link + '" target="_blank">Forum</a>';
+var twitter_josh_ref = '<a style="color: #264089;" href="' + twitter_josh_link + '" target="_blank">Twitter(proceduralguy)</a>';
+var twitter_lt_ref = '<a style="color: #264089;" href="' + twitter_lt_link + '" target="_blank">Twitter(LimitTheory)</a>';
+
+var no_posts_pattern = 'Nothing new from Josh :(';
+var one_post_pattern = 'New post on {0}';
+var two_posts_pattern = 'New posts on {0} and {1}';
+var three_posts_pattern = 'New posts on {0}, {1} and {2}';
+var four_posts_pattern = 'New posts on {0}, {1}, {2} and {3}';
+
+var posts = 0;
+var post_refs = new Array();
+
+var loaded = 0;
+
+// Adding wrapper to document
+var wrapperElement = document.createElement('div');
+var reference = document.getElementsByName("start_here")[0];
+reference.parentNode.insertBefore(wrapperElement, reference.nextSibling);
+
+if (new Date(GM_getValue('lastUpdate', '0')) > new Date() - 30*60*1000 && GM_getValue('cachedBanner', false)) {
+  wrapperElement.innerHTML = GM_getValue('cachedBanner');
+  return;
+}
+
+// Inject the string.format() function
+injectStringFormat();
 
 // Calling function to load posts from Kickstarter and the Forum
 invokeInfoLoading();
@@ -25,53 +53,55 @@ invokeInfoLoading();
 var bannerElement = document.createElement('div');
 var inner_html = document.createElement('span');
 updateBanner();
+wrapperElement.appendChild(bannerElement);
 bannerElement.appendChild(inner_html);
-
-// Adding banner to document
-var reference = document.getElementsByName("start_here")[0];
-reference.parentNode.insertBefore(bannerElement, reference.nextSibling);
 
 function updateBanner() {
   inner_html.style.display = 'inline-block';
   inner_html.style.verticalAlign = 'middle';
   inner_html.innerHTML = 'Loading...';
-  if (forum_loaded && kickstarter_loaded) {
+  if (loaded == 4) {
     // Info loaded
-    if (new_forum_post ? !new_kickstarter_post : new_kickstarter_post) {
-      // EITHER new forum post OR new kickstarter post
-      if (new_forum_post) {
-        // New forum post
-        inner_html.innerHTML = 'New post on the <a style="color: rgb(38, 84, 137);" href="' + forum_link + '" target="_blank">Forum</a>';
-      } else {
-        // New kickstarter post
-        inner_html.innerHTML = 'New post on <a style="color: rgb(137, 55, 38);" href="' + kickstarter_link + '" target="_blank">Kickstarter</a>';
-      }
-    } else if (new_forum_post && new_kickstarter_post) {
-      // New forum post AND new kickstarter post
-      inner_html.innerHTML = 'New posts on the <a style="color: rgb(38, 84, 137);" href="' + forum_link + '" target="_blank">Forum</a> and on <a style="color: rgb(137, 55, 38);" href="' + kickstarter_link + '" target="_blank">Kickstarter</a>';
-    } else {
-      // No new post
-      inner_html.innerHTML = 'Nothing new from Josh :(';
+    switch (posts) {
+      case 0:
+        inner_html.innerHTML = no_posts_pattern;
+        break;
+      case 1:
+        inner_html.innerHTML = one_post_pattern.format(post_refs);
+        break;
+      case 2:
+        inner_html.innerHTML = two_posts_pattern.format(post_refs);
+        break;
+      case 3:
+        inner_html.innerHTML = three_posts_pattern.format(post_refs);
+        break;
+      case 4:
+        inner_html.innerHTML = four_posts_pattern.format(post_refs);
+        break;
     }
   }
 
   bannerElement.style.width = '100%';
-  bannerElement.style.height = '50px';
   bannerElement.style.marginTop = '25px';
+  bannerElement.style.paddingBottom = '5px';
   bannerElement.style.textAlign = 'center';
   bannerElement.style.fontSize = '3em';
-  if (!forum_loaded || !kickstarter_loaded) {
+  if (loaded != 4) {
     // Info still loading
     bannerElement.style.backgroundColor = '#868508';
     bannerElement.style.color = '#383838';
-  } else if (new_forum_post || new_kickstarter_post) {
+  } else if (posts > 0) {
     // New post found
-    bannerElement.style.backgroundColor = 'rgb(55, 134, 8)';
-    bannerElement.style.color = 'rgb(65, 65, 65)';
+    bannerElement.style.backgroundColor = '#378608';
+    bannerElement.style.color = '#414141';
   } else {
     // Nothing new found
-    bannerElement.style.backgroundColor = 'rgb(155, 21, 21)';
-    bannerElement.style.color = 'rgb(113, 113, 113)';
+    bannerElement.style.backgroundColor = '#9B1515';
+    bannerElement.style.color = '#717171';
+  }
+  if (loaded == 4) {
+    GM_setValue('cachedBanner', wrapperElement.innerHTML);
+    GM_setValue('lastUpdate', new Date().toString());
   }
 }
 
@@ -82,9 +112,12 @@ var handler_forum = function handler_forum(response) {
   var doc = new DOMParser().parseFromString(response.responseText, "text/html");
 
   var timeElement = getElementsByClass('post', doc, 'div')[0].getElementsByTagName('dd')[0]; // Read out the time of the newest post from the website
-  if (Date.parse(timeElement.innerHTML) > new Date() - 14*24*60*60*1000) new_forum_post = true; // Compare to the 'newest' post
-  forum_loaded = true; // We checked the forum
-  if (kickstarter_loaded) updateBanner(); // If Kickstarter was checked too, we can update the banner
+  if (Date.parse(timeElement.innerHTML) > new Date() - 14*24*60*60*1000) { // Compare to the 'newest' post
+    post_refs[posts] = forum_ref;
+    posts++;
+  }
+  loaded++; // We checked the forum
+  if (loaded == 4) updateBanner(); // If the others were checked too, we can update the banner
 }
 
 // Same here
@@ -94,9 +127,27 @@ var handler_kickstarter = function handler_kickstarter(response) {
   var doc = new DOMParser().parseFromString(response.responseText, "text/html");
 
   var timeElement = getElementsByClass('post', doc, 'div')[0].getElementsByTagName('time')[0];
-  if (Date.parse(timeElement.getAttribute('datetime')) > new Date() - 14*24*60*60*1000) new_kickstarter_post = true;
-  kickstarter_loaded = true;
-  if (forum_loaded) updateBanner();
+  if (Date.parse(timeElement.getAttribute('datetime')) > new Date() - 14*24*60*60*1000) {
+    kickstarter_post = true;
+    post_refs[posts] = kickstarter_ref;
+    posts++;
+  }
+  loaded++;
+  if (loaded == 4) updateBanner();
+}
+
+var handler_twitter = function handler_twitter(response, josh) {
+  if (!(response.readyState == 4 && response.status == 200)) return;
+
+  var doc = new DOMParser().parseFromString(response.responseText, "text/html");
+
+  var timeElement = getElementsByClass('js-short-timestamp ', getElementsByClass('ProfileTweet', doc, 'div')[0], 'span')[0];
+  if (new Date(parseInt(timeElement.getAttribute('data-time'))*1000) > new Date() - 14*24*60*60*1000) { // Multiply the value of the timeElement date attribute by 1000 because its given in seconds
+    if (josh) post_refs[posts] = twitter_josh_ref; else post_refs[posts] = twitter_lt_ref;
+    posts++;
+  }
+  loaded++;
+  if (loaded == 4) updateBanner();
 }
 
 function invokeInfoLoading() {
@@ -117,19 +168,63 @@ function invokeInfoLoading() {
       onload: handler_kickstarter
     });
   }, 0);
+  
+  // Load Josh's Twitter page
+  setTimeout(function() {
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: twitter_josh_link,
+      onload: function(response) {
+        handler_twitter(response, true);
+      }
+    });
+  }, 0);
+  
+  // Load the LimitTheory Twitter page
+  setTimeout(function() {
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: twitter_lt_link,
+      onload: function(response) {
+        handler_twitter(response, false);
+      }
+    });
+  }, 0);
 }
 
 // Source: http://anyexample.com/webdev/javascript/javascript_getelementsbyclass_function.xml
 function getElementsByClass( searchClass, domNode, tagName) {
-	if (domNode == null) domNode = document;
-	if (tagName == null) tagName = '*';
-	var el = new Array();
-	var tags = domNode.getElementsByTagName(tagName);
-	var tcl = " "+searchClass+" ";
-	for(i=0,j=0; i<tags.length; i++) { 
-		var test = " " + tags[i].className + " ";
-		if (test.indexOf(tcl) != -1) 
-			el[j++] = tags[i];
-	} 
-	return el;
+  if (domNode == null) domNode = document;
+  if (tagName == null) tagName = '*';
+  var el = new Array();
+  var tags = domNode.getElementsByTagName(tagName);
+  var tcl = " "+searchClass+" ";
+  for(i=0,j=0; i<tags.length; i++) { 
+    var test = " " + tags[i].className + " ";
+    if (test.indexOf(tcl) != -1) 
+      el[j++] = tags[i];
+  } 
+  return el;
+}
+
+// Source: http://www.codeproject.com/Tips/201899/String-Format-in-JavaScript
+function injectStringFormat() {
+  String.prototype.format = function (args) {
+    var str = this;
+    return str.replace(String.prototype.format.regex, function(item) {
+      var intVal = parseInt(item.substring(1, item.length - 1));
+      var replace;
+      if (intVal >= 0) {
+        replace = args[intVal];
+      } else if (intVal === -1) {
+        replace = "{";
+      } else if (intVal === -2) {
+        replace = "}";
+      } else {
+        replace = "";
+      }
+      return replace;
+    });
+  };
+  String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 }
